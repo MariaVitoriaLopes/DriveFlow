4a8fddceb7ec5807a062906358226db0c21e5d2d
Bianca <biancaagantetiene@gmail.com>
2026-05-24 21:02:58 -0300

    Style - menu responsivo na lading page

--- frontend/src/app/components/layout/header/header.ts
+++ frontend/src/app/components/layout/header/header.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, LucideAngularModule ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+
+  menuAberto = false;
+
+  toggleMenu(): void {
+    this.menuAberto = !this.menuAberto;
+  }
+
+  fecharMenu(): void {
+    this.menuAberto = false;
+  }
+}
